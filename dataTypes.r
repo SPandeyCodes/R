ede8467aedd7f33e1d71376f1ed390d174329637
Sailesh Pandey <saileshpandey007007@gmail.com>
2021-03-22 23:30:07 +0530

    understand data types  and class function in R

---
+++ dataTypes.r
@@ -0,0 +1,27 @@
+
+#print() is used to print the values stored in the variables
+#class()is used to check the data types of the variables
+# numeric (10.50, 55, 787)
+x <- 10.50
+print(x)
+class(x) 
+
+# integer (1L, 55L, 100L, where the letter "L" declares this as an integer)
+x <- 100L
+print(x)
+class(x)
+
+# complex (9 + 3i, where "i" is the imaginary part)
+x <- 9i + 8
+print(x)
+class(x)
+
+# character/string (a.k.a. string) - ("k", "R is exciting", "FALSE", "11.5")
+x <- "R is amazing"
+print(x)
+class(x)
+
+# logical  (a.k.a. boolean) - (TRUE or FALSE)
+x <- TRUE
+print(x)
+class(x)
