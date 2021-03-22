3f43cfa61250b20e86a3a2df491bef325cdba9b1
Sailesh Pandey <saileshpandey007007@gmail.com>
2021-03-22 23:59:14 +0530

    understand maths in R

---
+++ maths.r
@@ -0,0 +1,24 @@
+# maths in R
+
+# addition operator
+a <- 5+5
+a
+
+# subtraction operator
+b <- 6-4
+b
+
+# max function to return the maximum value from the given values
+max(50,3,600,897,6,8)
+
+# min function to return the minimum value from the given values
+min(50,3,600,897,6,8)
+
+# returns the posive values only
+abs(-4.4)
+
+# returns the ceil value i.e. next greater value 
+ceiling(4.3)
+
+# returns the floor value i.e. previous lower value
+floor(4.3)
